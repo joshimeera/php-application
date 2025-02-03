700eaee8bd13b24d938759e4ea3ca42901e6f2b6
meera <meerajoshi966@gmail.com>
2025-02-03 11:05:18 +0545

    new

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo '<h1>सुन्दर शुदुरपश्चिम प्रदेश !</h1>';
 echo'<img src="mnr.jpg"width="700">';
-echo '<h1>सुन्दर शुदुरपश्चिम प्रदेश !</h1>';
+echo '<h1>Mahendranagar Kanchanpur !</h1>';
 
 ?>
